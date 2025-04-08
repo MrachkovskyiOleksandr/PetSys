650b9ca011de86a6747223b207638ba03116b7ec
MrachkovskyiOleksandr <knms1b22.mrachkovskyi@kpnu.edu.ua>
2025-04-08 12:40:46 +0300

    debug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
 	roles: [],
 	production: true,
 	appId: 'test',
-	url: '',
-	// url: 'https://webart.work',
+	// url: '',
+	url: 'https://webart.work',
 	meta: {
 		title: 'Web Art Work',
 		description:
