0c196c67eb6526f12ab31b93121b3929fe5fe951
MrachkovskyiOleksandr <knms1b22.mrachkovskyi@kpnu.edu.ua>
2025-05-26 23:17:06 +0300

    some styles rework

--- src/app/pages/user/profile/profile.component.ts
+++ src/app/pages/user/profile/profile.component.ts
@@ -12,10 +12,10 @@ interface ChangePassword {
 }
 
 @Component({
-    selector: 'app-profile',
-    templateUrl: './profile.component.html',
-    styleUrls: ['./profile.component.scss'],
-    standalone: false
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.scss'],
+	standalone: false
 })
 export class ProfileComponent {
 	readonly url = environment.url;
@@ -42,7 +42,6 @@ export class ProfileComponent {
 			{
 				name: 'Text',
 				key: 'name',
-				focused: true,
 				fields: [
 					{
 						name: 'Placeholder',
