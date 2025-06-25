91c6527c98f32d36be4e3c254cf1a205ab8b3e5c
MrachkovskyiOleksandr <knms1b22.mrachkovskyi@kpnu.edu.ua>
2025-06-25 12:32:10 +0300

    update app title

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
 	// url: '',
 	url: 'https://webart.work',
 	meta: {
-		title: 'Web Art Work',
+		title: 'PetSys',
 		description:
 			'An amazing solution to build web or mobile app for your business',
 		icon: 'https://webart.work/api/user/cdn/waw-logo.png'
